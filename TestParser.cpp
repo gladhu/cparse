c5cded73cb3245b7d7df1b95493d4319954b3c8f
gladhu <gladhu2016@qq.com>
2022-12-05 15:31:46 +0800

    测试用例

--- TestParser.cpp
+++ TestParser.cpp
@@ -8,6 +8,16 @@ struct STest
     int value;
 };
 
+packToken fibonacci(TokenMap scope) {
+    int N = scope["N"].asInt();
+    if (N == 0) return 0;
+    if (N == 1) return 1;
+
+    scope["N"] = N - 1;
+    int result = fibonacci(scope).asInt();
+    scope["N"] = N - 2;
+    return result + fibonacci(scope).asInt();
+}
 
 packToken TestPoint(TokenMap scope) {
     TokenMap& para = scope["this"].asMap();
@@ -39,6 +49,18 @@ int main() {
         std::cout << "value=" << test.value << " compare=" << c1.eval(paraMap) << std::endl; // 
     }
     
+
+    {
+        TokenMap& global = TokenMap::default_global();
+        global["fibonacci"] = CppFunction(&fibonacci, { "N", "div", "Name" }, "fib_name");
+        std::cout << "fibonacci(10)=" << calculator::calculate("fibonacci(10, 3, hello) > 0") << std::endl;
+    }
+
+    {
+        TokenMap vars;
+        vars["fibonacci"] = CppFunction(&fibonacci, { "N", "div", "Name" });
+        std::cout << "fibonacci(10)=" << calculator::calculate("fibonacci(10, 3, hello) > 0", &vars) << std::endl;
+    }
 
     return 0;
 }
