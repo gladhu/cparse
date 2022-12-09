1a790bdbbc6e53c391ff26329c39a05598403718
gladhu <gladhu2016@qq.com>
2022-12-09 11:49:08 +0800

    测试用例

--- TestParser.cpp
+++ TestParser.cpp
@@ -26,6 +26,22 @@ packToken TestPoint(TokenMap scope) {
     return pTest->value;
 }
 
+
+packToken TestGroupFun(TokenMap scope) {
+    auto Key = scope["Key"].asString();
+    auto Div = scope["Div"].asInt();
+    auto Index = 0;
+    auto& Paras = scope["this"].asMap();
+    if (Paras.find("Index"))
+    {
+        Index = Paras["Index"].asInt();
+    }
+
+    std::cout << "Key=" << Key << " Div=" << Div << " Index=" << Index  << std::endl;
+
+    return 0;
+}
+
 int main() {
     
     // 将函数定义放到全局
@@ -50,16 +66,50 @@ int main() {
     }
     
 
-    {
-        TokenMap& global = TokenMap::default_global();
-        global["fibonacci"] = CppFunction(&fibonacci, { "N", "div", "Name" }, "fib_name");
-        std::cout << "fibonacci(10)=" << calculator::calculate("fibonacci(10, 3, hello) > 0") << std::endl;
-    }
+    // {
+    //     TokenMap& global = TokenMap::default_global();
+    //     global["fibonacci"] = CppFunction(&fibonacci, { "N", "div", "Name" }, "fib_name");
+    //     std::cout << "fibonacci(10)=" << calculator::calculate("fibonacci(10, 3, hello) > 0") << std::endl;
+    // }
+
+    // {
+    //     TokenMap vars;
+    //     vars["fibonacci"] = CppFunction(&fibonacci, { "N", "div", "Name" });
+    //     vars["N"] = 12;
+    //     std::cout << "fibonacci(10)=" << calculator::calculate("fibonacci(10, 3, hello)", &vars) << std::endl;
+    // }
+
+
+    //{
+    //    TokenMap vars;
+    //    vars["TestGroup"] = CppFunction(&TestGroupFun, { "Key", "Div", "Name" });
+    //    std::cout << "TestGroup(key, div)=" << calculator::calculate("TestGroup(key, 3)", &vars) << std::endl;
+    //}
 
     {
         TokenMap vars;
-        vars["fibonacci"] = CppFunction(&fibonacci, { "N", "div", "Name" });
-        std::cout << "fibonacci(10)=" << calculator::calculate("fibonacci(10, 3, hello) > 0", &vars) << std::endl;
+        vars["TestGroup"] = CppFunction(&TestGroupFun, { "Key", "Div", "Index2" });
+        //global["TestGroup"] = CppFunction(&TestGroupFun, { "Key", "Div", "Name" });
+
+        calculator calc;
+        calc.compile("TestGroup(key, 3)");
+
+        for (int i=0; i<5; i++)
+        {
+            vars["Index"] = i;
+            try
+            {
+                calc.eval(vars, true);
+
+            }
+            catch (std::exception* e)
+            {
+                auto exp = e->what();
+            }
+        }
+
+
+        //std::cout << "TestGroup(key, div)=" << calculator::calculate("TestGroup(key, 3)", &vars) << std::endl;
     }
 
     return 0;
